f62bd599860223e50e028a4209b1b6523d141565
strawhathacker <ntahsp@gmail.com>
2023-07-24 14:27:29 +0300

    Copy to clickboard button

---
+++ src/components/Custom/CopyButton.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Copy } from "carbon-icons-svelte";
+
+  export let size = 24;
+  export let text: string;
+
+  const clipboard = (text: string) => navigator.clipboard.writeText(text);
+</script>
+
+<button class="copy-btn" on:click|preventDefault={() => clipboard(text)}>
+  <Copy {size} />
+</button>
+
+<style>
+  .copy-btn {
+    transition: opacity 0.2s;
+  }
+
+  .copy-btn:hover {
+    opacity: 0.7;
+  }
+</style>
