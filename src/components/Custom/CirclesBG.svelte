6ecd0a784aa6658344e3ed34fe96feb14044554b
strawhathacker <ntahsp@gmail.com>
2023-07-24 14:27:43 +0300

    Circles no longer break on mobile

--- src/components/Custom/CirclesBG.svelte
+++ src/components/Custom/CirclesBG.svelte
@@ -1,25 +1,27 @@
-<div class="bg-circles">
+<script>
+  import { page } from "$app/stores";
+
+  let show = $page.url.pathname !== "/";
+
+  $: {
+    show = $page.url.pathname !== "/";
+  }
+</script>
+
+<div class:hidden={show}>
   <div class="c-1" />
   <div class="c-2" />
 </div>
 
 <style>
-  .bg-circles {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    z-index: -1;
-  }
-
   .c-1,
   .c-2 {
     background-color: transparent;
     border-radius: 50%;
     opacity: 0.02;
-    position: fixed;
+    position: absolute;
     border: 5px solid white;
+    z-index: -1;
   }
 
   .c-1 {
@@ -34,5 +36,13 @@
     aspect-ratio: 1/1;
     top: 40px;
     left: calc(8% + -40px);
+  }
+
+  /* Mobile only */
+  @media only screen and (hover: none) and (pointer: coarse) {
+    .c-1,
+    .c-2 {
+      display: none;
+    }
   }
 </style>
